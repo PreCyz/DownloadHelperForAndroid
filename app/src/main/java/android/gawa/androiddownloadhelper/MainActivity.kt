36515c8e6a2e5adf5c2bdb0da9822760dd521b6c
PreCyz <precpaw@op.pl>
2018-07-10 18:49:09 +0200

    Adding coroutines.

--- app/src/main/java/android/gawa/androiddownloadhelper/MainActivity.kt
+++ app/src/main/java/android/gawa/androiddownloadhelper/MainActivity.kt
@@ -1,12 +1,13 @@
 package android.gawa.androiddownloadhelper
 
+import android.gawa.androiddownloadhelper.component.mapper.TorrentResponseMapper
 import android.gawa.androiddownloadhelper.component.settings.*
+import android.gawa.androiddownloadhelper.component.web.WebClient
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import khttp.responses.Response
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
+import kotlinx.coroutines.experimental.async
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,32 +17,24 @@ class MainActivity : AppCompatActivity() {
 
         helloWorldTxt.text = "Pawel Gawedzki"
 
-        var clickCounter = 0
 
-        getRequest()
-
-        downloadFavouritesBtn.setOnClickListener{
+        downloadFavouritesBtn.setOnClickListener {
 
             val appSetting = AppSettings(ServerSetup(), ApiSetup(), RequestSetup(), FilterSetup(), OtherSetup())
             Toast.makeText(this@MainActivity, "haha it works.", Toast.LENGTH_SHORT).show()
-            if (clickCounter % 2 == 0) {
-                helloWorldTxt.text = appSetting.apiSetup.toString()
-            } else {
-                helloWorldTxt.text = appSetting.otherSetup.toString()
-            }
-            clickCounter++
-        }
-    }
 
-    fun getRequest() {
-        val response : Response = khttp.get(
-                url = "https://eztv.ag/api/get-torrents",
-                params = mapOf("limit" to "10", "page" to "1"))
-        if(response.statusCode == 200) {
-            val obj : JSONObject = response.jsonObject
-            println(obj["someProperty"])
-        } else {
-            println("Response code: ${response.statusCode}")
+            val url = "https://eztv.ag/api/get-torrents"
+            val params = mapOf("limit" to "2", "page" to "1")
+
+            val response = async {
+                WebClient().getRequest(url, params)
+            }
+
+            val titles = TorrentResponseMapper().map(response.await())
+
+            helloWorldTxt.text = titles.torrents.joinToString(",") { torrentDetail -> torrentDetail.title }
+            /*helloWorldTxt.text = appSetting.apiSetup.toString()
+            helloWorldTxt.text = appSetting.otherSetup.toString()*/
         }
     }
 }
