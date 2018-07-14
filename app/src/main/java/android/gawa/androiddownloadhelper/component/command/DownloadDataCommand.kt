6f48e56e1eef525b6ba94269d0c1ba4aae6088f9
PreCyz <precpaw@op.pl>
2018-07-14 08:08:54 +0200

    Defining maxLimit.

--- app/src/main/java/android/gawa/androiddownloadhelper/component/command/DownloadDataCommand.kt
+++ app/src/main/java/android/gawa/androiddownloadhelper/component/command/DownloadDataCommand.kt
@@ -7,20 +7,21 @@ internal class DownloadDataCommand(private val numberOfTorrents: Int) : Command 
 
     private var nextCommand : Command? = null
     private val url = "https://eztv.ag/api/get-torrents"
+    private val maxLimit = 100
 
     override fun next(command: Command) {
         this.nextCommand = command
     }
 
     override fun execute(commandResult: CommandResult) {
-        val counter = 1 + (numberOfTorrents / 100)
-        if (counter == 1 && numberOfTorrents % 100 == 0) {
+        val counter = 1 + (numberOfTorrents / maxLimit)
+        if (counter == 1 && numberOfTorrents % maxLimit == 0) {
             return
         }
         for (i: Int in 1..counter) {
-            var limit = numberOfTorrents % 100
+            var limit = numberOfTorrents % maxLimit
             if (i * 100 < numberOfTorrents) {
-                limit = 100
+                limit = maxLimit
             }
             val params = mapOf("limit" to limit.toString(), "page" to i.toString())
             val response = WebClient().getRequest(url, params)
